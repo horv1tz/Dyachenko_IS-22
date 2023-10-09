9ac73213d2e0b88413f10929115844a6c28a1e71
VacheslavAksenovIS-25 <aksenovvaceslav2@gmail.com>
2023-10-09 18:45:46 +0300

    Edit files

--- PZ21/main.py
+++ PZ21/main.py
@@ -1,1 +1,3 @@
-print(f'Размер файла в килобайтах << { int(input("Введите размер файла в байтах >> ")) // 1024}')
+input_weight_b = int(input("Введите размер файла в байтах >> "))
+weight_kb = input_weight_b // 1024
+print(f'Размер файла в килобайтах << {weight_kb}KB')
